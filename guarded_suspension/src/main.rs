36efcf1d25e405bf2d8618d347c40ff938f16287
Iede Snoek <info@esoxsolutions.nl>
2024-04-16 05:40:51 +0200

    code improvements

--- guarded_suspension/src/main.rs
+++ guarded_suspension/src/main.rs
@@ -36,17 +36,32 @@ impl GuardedGarage {
     }
 
     fn park(&self,car:ExpensiveCar) {
-        let mut queue=self.queue.lock().unwrap();
-        queue.push(car);
-        self.guard.notify_one();
+        let queue_result=self.queue.lock();
+        match queue_result {
+            Ok(mut queue) => {
+                queue.push(car);
+                self.guard.notify_one();
+            }
+            Err(poisoned) => {
+                println!("Error occurred: {:?}",poisoned);
+            }
+        }
     }
 
-    fn get(&self)->ExpensiveCar {
-        let mut queue=self.queue.lock().unwrap();
-        while queue.is_empty() {
-            queue=self.guard.wait(queue).unwrap();
+    fn get(&self)->Option<ExpensiveCar> {
+        let queue_result=self.queue.lock();
+        match queue_result {
+            Ok(mut queue) => {
+                while queue.is_empty() {
+                    queue=self.guard.wait(queue).unwrap();
+                }
+                Some(queue.remove(0))
+            }
+            Err(poisoned) => {
+                println!("Error occurred: {:?}",poisoned);
+                None
+            }
         }
-        queue.remove(0)
     }
 
 
@@ -70,13 +85,30 @@ fn main() {
         let guarded_garage=Arc::clone(&guarded_garage);
         thread::spawn(move || {
             for _ in 0..10 {
-                let expensive_car=guarded_garage.get();
-                println!("got a car: {}",expensive_car);
+                if let Some(expensive_car)=guarded_garage.get() {
+                    println!("got a car: {}",expensive_car);
+                }
+
             }
 
         })
     };
-
-    producer.join().unwrap();
-    consumer.join().unwrap();
+    let producer_result=producer.join();
+    match producer_result {
+        Ok(_) => {
+            println!("Producer thread finished");
+        }
+        Err(e) => {
+            println!("Error occurred: {:?}",e);
+        }
+    }
+    let consumer_result=consumer.join();
+    match consumer_result {
+        Ok(_) => {
+            println!("Consumer thread finished");
+        }
+        Err(e) => {
+            println!("Error occurred: {:?}",e);
+        }
+    }
 }
